167fa9149bc9d3c5deb527c82957f52a25990a6b
Monet <monetpaul23@gmail.com>
2023-12-21 17:33:51 -0500

    working on   random  data  for  moment section

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 //www.thecocktaildb.com/api/json/v1/1/random.php
+
+ const momentElement= document.querySelector('.section-moment')
+
+   async function randomMoment(){
+     const randomCocktail= await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+
+     const randomCocktailData=  await randomCocktail.json()
+
+     
+      console.log( randomCocktailData)
+   }
+
+   randomMoment();
+
+   //15  for  both
+    function cocktailHTML(cocktail){
+         return `
+         <figure><img  src="${cocktail.strDrinkThumb}"/></figure>
+            <h3>${cocktail.strDrink} <em>(${cocktail.strAlcoholic})</em></h3>
+            <h4>Ingredients</h4>
+            <p>${cocktail.strIngredient1 || ""}${cocktail.strMeasure1 || ""}
+            ${cocktail.strIngredient2 || ""}${cocktail.strMeasure2 || ""}
+            ${cocktail.strIngredient3 || ""}${cocktail.strMeasure3 || ""}
+            ${cocktail.strIngredient1 || ""}${cocktail.strMeasure1 || ""}
+            
+            
+            </p>
+            <h4>Instuction</h4>
+            <p></p>
+        `
+    }
