b0c12c5399da16d0e1eaaf4d63aac2eb8e3e2946
Monet <monetpaul23@gmail.com>
2023-12-23 07:37:23 -0500

    finish layout  for  special and moment

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 //www.thecocktaildb.com/api/json/v1/1/random.php
 
- const momentElement= document.querySelector('.section-moment')
+ const momentElement= document.querySelector('#moment')
 
    async function randomMoment(){
      const randomCocktail= await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 
      const randomCocktailData=  await randomCocktail.json()
+     const oneCocktail= randomCocktailData.drinks
+     console.log(oneCocktail) 
 
-     
-      console.log( randomCocktailData)
+   momentElement.innerHTML= oneCocktail.map(cocktail => cocktailHTML(cocktail)).join("");
+    
    }
 
    randomMoment();
 
-   //15  for  both
+   
     function cocktailHTML(cocktail){
          return `
-         <figure><img  src="${cocktail.strDrinkThumb}"/></figure>
+         <figure class="cocktail--img-container" ><img  src=${cocktail.strDrinkThumb} /></figure>
             <h3>${cocktail.strDrink} <em>(${cocktail.strAlcoholic})</em></h3>
+            <h4>${cocktail.strGlass} is needed</h4>
+            
             <h4>Ingredients</h4>
-            <p>${cocktail.strIngredient1 || ""}${cocktail.strMeasure1 || ""}
-            ${cocktail.strIngredient2 || ""}${cocktail.strMeasure2 || ""}
-            ${cocktail.strIngredient3 || ""}${cocktail.strMeasure3 || ""}
-            ${cocktail.strIngredient1 || ""}${cocktail.strMeasure1 || ""}
-            
-            
-            </p>
-            <h4>Instuction</h4>
-            <p></p>
+            <ul class="ingredients-list">
+            <li>${cocktail.strIngredient1 || ""} ${cocktail.strMeasure1 || ""}</li>
+            <li> ${cocktail.strIngredient2 || ""} ${cocktail.strMeasure2 || ""}</li>
+            <li>${cocktail.strIngredient3 || ""} ${cocktail.strMeasure3 || ""}</li>
+            <li>${cocktail.strIngredient4 || ""} ${cocktail.strMeasure4 || ""}</li>
+            <li>${cocktail.strIngredient5 || ""} ${cocktail.strMeasure5 || ""}</li>
+            <li>${cocktail.strIngredient6 || ""} ${cocktail.strMeasure6 || ""}</li>
+            <li>${cocktail.strIngredient7 || ""} ${cocktail.strMeasure7 || ""}</li>
+            <li>${cocktail.strIngredient8 || ""} ${cocktail.strMeasure8 || ""}</li>
+            <li>${cocktail.strIngredient9 || ""} ${cocktail.strMeasure9 || ""}</li>
+            <li>${cocktail.strIngredient10 || ""} ${cocktail.strMeasure10 || ""}</li>
+            <li>${cocktail.strIngredient11 || ""} ${cocktail.strMeasure11 || ""}</li>
+            <li>${cocktail.strIngredient12 || ""} ${cocktail.strMeasure12 || ""}</li>
+            <li>${cocktail.strIngredient13 || ""} ${cocktail.strMeasure13 || ""}</li>
+            <li>${cocktail.strIngredient14 || ""} ${cocktail.strMeasure14 || ""}</li>
+            <li>${cocktail.strIngredient15 || ""} ${cocktail.strMeasure15 || ""}</li>
+            </ul>
+            <h4>Instruction</h4>
+            <p class="instruc">${cocktail.strInstructions}</p>
         `
     }
